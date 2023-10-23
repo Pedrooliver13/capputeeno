b35f8b9281f03b50ac3c0ab745efc986f3d0c8a7
pedrooliver13 <pedro_oliver13@hotmail.com>
2023-10-23 17:02:34 -0300

    feat: add responsive in header component

--- src/components/core/header/styles.ts
+++ src/components/core/header/styles.ts
@@ -14,45 +14,57 @@ export const HeaderContainer = styled.header`
         display: flex;
         align-items: center;
         justify-content: space-between;
-        height: 80px;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        flex-wrap: wrap;
+        gap: 2.4rem;
 
         &__logo {
+          flex: 1;
           font-family: ${theme.fonts.logoFont};
           font-size: ${theme.textSizes['logo-l']};
           color: ${theme.colors['base-logo']};
         }
+
+        &__search {
+          max-width: 350px;
+          width: 100%;
+
+          @media (max-width: ${theme.breakpoints.MD}) {
+            order: 3;
+            min-width: 100%;
+          }
+        }
+
+        &__cart {
+          position: relative;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 0.8rem;
+          border: none;
+          border-radius: 6px;
+
+          @media (max-width: ${theme.breakpoints.MD}) {
+            order: 2;
+          }
+
+          &--count {
+            position: absolute;
+            bottom: 0px;
+            right: 0px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 1.7rem;
+            height: 1.7rem;
+            border-radius: 50%;
+            font-weight: 500;
+            background-color: ${({ theme }): string => theme.colors.red};
+            color: ${({ theme }): string => theme.colors.white};
+            font-size: ${({ theme }): string => theme.textSizes['text-xs']};
+          }
+        }
       }
     `}
-`;
-
-export const HeaderDashboard = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 4rem;
-
-  .cart {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0.8rem;
-    border: none;
-    border-radius: 6px;
-
-    &__count {
-      position: absolute;
-      bottom: 0px;
-      right: 0px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 1.7rem;
-      height: 1.7rem;
-      border-radius: 50%;
-      font-weight: 500;
-      background-color: ${({ theme }): string => theme.colors.red};
-      color: ${({ theme }): string => theme.colors.white};
-      font-size: ${({ theme }): string => theme.textSizes['text-xs']};
-    }
-  }
 `;
